26ff4c96ac567c1def725875a912407e771d8e28
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2022-12-22 10:10:13 +0100

    Add MC for cluster formulation 22/12/2022

--- utils/monte_carlo.jl
+++ utils/monte_carlo.jl
@@ -12,9 +12,19 @@ function getScenario(failureRate)
         d = Binomial(1, fail)
         append!(T, rand(d, 1))
     end
-    return(T)
+    return T
 end
 
+
+"""
+getScenarioK
+
+This function return K scenarios for the recourse problem
+
+# Parameters
+* `failureRate` : list of the probability that each cycle fails (of shape |C|)
+* `K` : scenario amount
+"""
 
 function getScenarioK(failureRate, K)
     card_cycle = length(failureRate)
@@ -22,13 +32,50 @@ function getScenarioK(failureRate, K)
     for i in 1:1:K
         T[:, i] = getScenario(failureRate)
     end
-    return(T)
+    return T
 end
 
 
-function getClusterScenario(kep_graph)
 
+"""
+getScenarioCluster
+
+This function return a scenario for the recourse problem with cluster formulation
+
+# Parameters
+* `kep_graph` : instance graph
+"""
+function getScenarioCluster(kep_graph)
+    n = nv(kep_graph)
+    S = zeros(n, n)
+    for i in 1:1:n
+        for j in 1:1:n
+            if has_edge(kep_graph, i, j)
+                d = Binomial(1, get_prop(kep_graph, Edge((i, j)), :failure))
+                S[i, j] = rand(d, 1)
+            end 
+        end
+    end
+    return S
 end
-
-
 ;
+
+
+"""
+getScenarioK
+
+This function return K scenarios for the recourse problem with cluster formulation
+
+# Parameters
+* `kep_graph` : instance graph
+"""
+
+function getScenarioClusterK(kep_graph, K)
+    n = nv(kep_graph) 
+    Sk = zeros(n, n, K) 
+    
+    for i in 1:1:K
+        Sk[:, :, i] =  getScenarioCluster(kep_graph)
+    end
+    return Sk
+end
