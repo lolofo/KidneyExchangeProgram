95b502e9b243503db9860901a74404178173876d
lolofo <loicfosse65310@gmail.com>
2023-01-05 21:15:07 +0100

    modifications in the recourse problem

--- src/stochastic_framework/recourse_problem.jl
+++ src/stochastic_framework/recourse_problem.jl
@@ -96,7 +96,7 @@ end
 """
 recourseClusterProblem
 
-This function will solve the recourse problem with cycle formulation for multiple scenarios
+This function will create the recourse problem with cycle formulation for multiple scenarios
 
 # Parameters
 * `x` : solution au MP
@@ -108,10 +108,7 @@ This function will solve the recourse problem with cycle formulation for multipl
 
 # Returns :
 This functions will return
-* the model : The model will allow us to access some other values later.
-* the duals solutions : the dual solution will allow us to update the master problem in the Benders decomposition.
-The dual solutions are decomposed into three groups lambda, mu and Delta which corresponds to the different constraints of the recourse
-problem.
+* `model` : the model of the recourseProblem
 """
 function recourseClusterProblem(x, ksi, C, vertic_cycles, U, cycles)
     
@@ -148,18 +145,40 @@ function recourseClusterProblem(x, ksi, C, vertic_cycles, U, cycles)
         cons = @constraint(model, sum(y[c] for c in C_v)<=1)
         set_name(cons, "cons_mu_"*string(v))
     end
+    return(model)
+end
 
+
+"""
+    solveRecourseClusterProblem
+
+The previous function was made to create the recourse problem.
+This new function will solve the recourse problem and get the dual of the problem
+
+# Parameters
+# `model` : the recourse problem
+* `x` : solution au MP
+* `ksi` : random variable giving the success/fail of the cross test (one realisation of the random variable)
+* `C` : list of cycle index
+* `vertic_cycles` : Dictionary with {"graph vertices" : "lists of cycle which implie the keys"}
+* `U` : list of utility of each cycle
+* `cycle` : an exaustive list of the cycle
+
+# Returns :
+This functions will return
+* the duals solutions : the dual solution will allow us to update the master problem in the Benders decomposition.
+The dual solutions are decomposed into three groups lambda, mu and Delta which corresponds to the different constraints of the recourse
+problem.
+"""
+function solveRecourseClusterProblem(model, x, ksi, C, vertic_cycles, U, cycles)
     # optimisation 
     # handle exception because julia error aren't good.
-
     try
         optimize!(model)
     catch e
         println("optimization error : problem during the optimization part")
         println(e.msg)
     end
-    
-
     ### Now lets recover all the duals solutions ###
 
     # the different dual solutions.
@@ -199,9 +218,42 @@ function recourseClusterProblem(x, ksi, C, vertic_cycles, U, cycles)
     end
 
     return Dict(
-        "model" => model,
         "dual" => Dict("dual_lambda" => dual_lambda, "dual_mu" => dual_mu, "dual_delta" => dual_delta)
     )
 end
 ;
 
+
+"""
+    modifyRecourseClusterProblem
+
+This function will modify the recourseClusterProblem for the L-shape method.
+The objective is, at each iteration of the L-shape method, to not create again another recourse problem.
+There is only one constraint that need to be modified for our problem.
+
+# Parameters
+# `model` : the model of the recourse problem
+# `x` : the first level value variable
+# `C` : the cycle's indexes
+# `ksi` : the scenario we are dealing with
+"""
+function modifyRecourseClusterProblem(model, x, C, ksi)
+    for c in C
+        current_cycle = cycles[c]
+        for k in 1:1:length(current_cycle)
+            i = current_cycle[k]
+            j = Nothing
+            if k==length(current_cycle)
+                j = current_cycle[1] 
+            else
+                j = current_cycle[k+1]
+            end
+            # delete the constraint
+            delete(model, constraint_by_name(model, "cons_lambda_"*string(c)*"_"*string(i)*"_"*string(j)))
+
+            # add the new constraint
+            cons = @constraint(model, model[:y][c] <= x[i, j]*ksi[i, j])
+            set_name(cons, "cons_lambda_"*string(c)*"_"*string(i)*"_"*string(j))
+        end 
+    end
+end;
