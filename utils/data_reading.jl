b161080892252e5d54fd8e3c7d6e18ce403f5b35
b-ptiste <75781257+b-ptiste@users.noreply.github.com>
2023-01-24 10:25:29 +0100

    24/01/2023

--- utils/data_reading.jl
+++ utils/data_reading.jl
@@ -108,3 +108,58 @@ function get_failure_rates(kep_graph::MetaDiGraph, distribution::String)
     end
 end
 ;
+
+
+"""
+    addSpecificEdges
+
+Add specific edges
+
+# Parameters
+* `kep_graph::MetaDiGraph` : graph describing the pairs and compatibilities
+* `list_edges` : edges to add
+* `fail` : failure rate to affect
+* `weight` : weight to affect
+"""
+
+function addSpecificEdges(kep_graph, list_edges, fail, weight)
+    n_row = size(list_edges)[1]
+    print(n_row)
+    for i in 1:1:n_row
+        add_edge!(kep_graph, list_edges[i, 1], list_edges[i, 2], :weight, weight)
+        set_prop!(kep_graph, Edge(list_edges[i, 1], list_edges[i, 2]), :failure, fail)
+    end
+    return kep_graph
+end;
+
+
+"""
+    addSpecificEdges
+
+Add specific edges
+
+# Parameters
+* `kep_graph::MetaDiGraph` : graph describing the pairs and compatibilities
+* `ratio` : ratio 
+* `fail` : failure rate to affect
+* `weight` : weight to affect
+"""
+
+function addRandomEdges(kep_graph, ratio, fail, weight)
+    nb_nodes = nv(kep_graph)
+    nb_edges = ne(kep_graph)
+    nb_it = floor(nb_edges * ratio)
+    nb_add = 1
+    while(nb_add < nb_it)
+        s = trunc(Int, rand(1:nb_nodes))
+        d = trunc(Int, rand(1:nb_nodes))
+        cond = ((s != d) && (~has_edge(kep_graph, s, d)))
+        if cond
+            add_edge!(kep_graph, s, d, :weight, weight)
+            set_prop!(kep_graph, Edge(s, d), :failure, fail)
+            nb_add += 1
+        end
+    end
+    return kep_graph
+end
+; 
